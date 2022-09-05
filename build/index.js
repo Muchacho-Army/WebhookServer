df519631d5b4113e60bfa1caa41d126a161ee2fc
Builder <actions@github.com>
2022-09-05 11:19:52 +0000

    Publish build

--- build/index.js
+++ build/index.js
@@ -5,7 +5,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const dotenv_1 = __importDefault(require("dotenv"));
-const webhook_1 = require("./webhook");
+const webhook_1 = __importDefault(require("./webhook"));
 // @ts-ignore
 const youtube_notification_1 = __importDefault(require("youtube-notification"));
 dotenv_1.default.config();
@@ -15,11 +15,10 @@ const baseUrl = `${process.env.SERVER}:${port}`;
 const YT_LUSOR = "UCutxBsHkawd5e_0rsWNYHpw";
 const YT_EHRENLORD = "UCAAG4XG6FI6WtfJX0e5HTxg";
 const channels = [YT_LUSOR, YT_EHRENLORD];
-const webhook = new webhook_1.DiscordWebhook(process.env.WEBHOOK_URL);
-webhook.modify({
-    username: "YouTube",
-    avatar_url: "https://cdn.discordapp.com/app-assets/769568156309389332/1015627360156008448.png",
-});
+const yt_webhook = new webhook_1.default(process.env.WEBHOOK_URL);
+yt_webhook.modify({ username: "YouTube", avatar_url: "https://cdn.discordapp.com/app-assets/769568156309389332/1015627360156008448.png" });
+const tv_webhook = new webhook_1.default(process.env.WEBHOOK_URL);
+tv_webhook.modify({ username: "Twitch", avatar_url: "https://cdn.discordapp.com/app-assets/769568156309389332/1016299248746246195.png" });
 const notifier = new youtube_notification_1.default({
     hubCallback: `${baseUrl}/youtube/notifications`,
 });
@@ -34,7 +33,7 @@ notifier.on("subscribe", (data) => {
 notifier.on("notified", (data) => {
     if (new Date(new Date(data.updated).getTime() - 1000 * 60) <= new Date(data.published)) {
         console.log(`${data.channel.name} published ${data.video.title}`);
-        webhook.send({
+        yt_webhook.send({
             content: `Hey <@&880108096788234300>,\n${data.channel.id === YT_LUSOR ? "die **Lusors** haben" : "**Doppelter Ehrenlord** hat"} ein neues Video hochgeladen! 🤙\n${data.video.link}`,
         });
     }
--- build/webhook.js
+++ build/webhook.js
@@ -1,58 +1,44 @@
 "use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.DiscordWebhook = void 0;
+const node_fetch_1 = __importDefault(require("node-fetch"));
 class DiscordWebhook {
     constructor(webhookUrl) {
         this.webhookUrl = webhookUrl;
         this.webhookData = {};
     }
-    modify(data) {
-        return __awaiter(this, void 0, void 0, function* () {
-            const webhookData = this.webhookData;
-            this.webhookData = Object.assign(Object.assign({}, webhookData), data);
-            return this.webhookData;
+    async modify(data) {
+        const webhookData = this.webhookData;
+        this.webhookData = { ...webhookData, ...data };
+        return this.webhookData;
+    }
+    async send(data) {
+        const webhookData = this.webhookData;
+        (0, node_fetch_1.default)(this.webhookUrl, {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({ ...webhookData, ...data }),
         });
     }
-    send(data) {
-        return __awaiter(this, void 0, void 0, function* () {
-            const webhookData = this.webhookData;
-            fetch(this.webhookUrl, {
-                method: "POST",
-                headers: { "Content-Type": "application/json" },
-                body: JSON.stringify(Object.assign(Object.assign({}, webhookData), data)),
-            });
-        });
-    }
-    get() {
-        return __awaiter(this, void 0, void 0, function* () {
-            return new Promise((resolve, reject) => __awaiter(this, void 0, void 0, function* () {
-                const response = yield fetch(this.webhookUrl);
-                if (!response.ok) {
-                    return reject("Webhook doesn't exist");
-                }
-                resolve(response.json());
-            }));
-        });
-    }
-    isValid() {
-        return __awaiter(this, void 0, void 0, function* () {
-            try {
-                yield this.get();
-                return true;
+    async get() {
+        return new Promise(async (resolve, reject) => {
+            const response = await (0, node_fetch_1.default)(this.webhookUrl);
+            if (!response.ok) {
+                return reject("Webhook doesn't exist");
             }
-            catch (_a) {
-                return false;
-            }
+            resolve(response.json());
         });
+    }
+    async isValid() {
+        try {
+            await this.get();
+            return true;
+        }
+        catch {
+            return false;
+        }
     }
 }
-exports.DiscordWebhook = DiscordWebhook;
+exports.default = DiscordWebhook;
